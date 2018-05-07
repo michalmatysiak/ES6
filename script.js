c97ffeb59e91a5e91f8e1b81647332ac8375470b
michalmatysiak <37140076+michalmatysiak@users.noreply.github.com>
2018-05-07 18:08:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 // 1.
-const a = 'Hello ';
-const b = 'World!!!';
+
+const a = "Hello ";
+const b = "World!!!";
+
 console.log(`${a}, ${b}`);
 
 // 2.
-const multiply = (a=1,b=1) => console.log(`${a * b}`);
-multiply(3,7);
-multiply(6);
-multiply();
 
-// 3.			
-const average = (...numbers) => console.log(numbers.reduce((a, b) => a + b)/numbers.length);
+const multiply = (a = 1, b = 1) => a * b;
+
+console.log(multiply(3, 7));
+console.log(multiply(6));
+console.log(multiply());
+
+// 3.
+
+const average = (...numbers) =>
+	console.log(numbers.reduce((a, b) => a + b) / numbers.length);
 average(1);
-average(1,3);
-average(1,3,6,6);
+average(1, 3);
+average(1, 3, 6, 6);
 
 // 4.
+
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
-const average = (...grades) => console.log(grades.reduce((a, b) => a + b)/grades.length);
-console.log(average(...grades));
+
+const avg = (...grades) =>
+	console.log(grades.reduce((a, b) => a + b) / grades.length);
+console.log(avg(...grades));
 
 // 5.
-const sth = [2, 3, 'Jurekk', false, 'Kowalski'];
+
+const sth = [2, 3, "Jurekk", false, "Kowalski"];
 const [, , firstname, , lastname] = sth;
+
 console.log(`${firstname} ${lastname}`);
